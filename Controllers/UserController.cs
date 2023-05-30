610a4217053b7d8ce93594346c0490312c225056
Denys Samoilenko <denniksam@gmail.com>
2023-05-30 17:05:08 +0300

    form ---1

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_111.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+    }
+}
