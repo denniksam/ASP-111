37594bbd6902042fce2fc86e82e49d4b83a64ea4
Denys Samoilenko <denniksam@gmail.com>
2023-05-30 19:41:14 +0300

    form --- 3

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,16 +1,108 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP_111.Data;
+using ASP_111.Models.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace ASP_111.Controllers
 {
     public class UserController : Controller
     {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public ViewResult SignUp()
+
+        public ViewResult SignUp(SignUpFormModel? formModel)
         {
-            return View();
+            SignUpViewModel viewModel;
+            if(Request.Method == "POST" && formModel != null)
+            {
+                // передача формы
+                viewModel = ValidateSignUpForm(formModel);
+                viewModel.FormModel = formModel;
+            }
+            else
+            {
+                // первый заход - начало заполнения формы
+                viewModel = new();
+                viewModel.FormModel = null;  // нечего проверять
+            }
+            return View(viewModel);  // передаем модель в представление
+        }
+
+        private SignUpViewModel ValidateSignUpForm(SignUpFormModel formModel)
+        {
+            SignUpViewModel viewModel = new();
+            if(String.IsNullOrEmpty(formModel.Login)) 
+            {
+                viewModel.LoginMessage = "Логин не может быть пустым";
+            }
+            else if(formModel.Login.Length < 3)
+            {
+                viewModel.LoginMessage = "Логин слишком короткий (3 символа минимум)";
+            }
+            else if(_dataContext.Users.Any(u => u.Login == formModel.Login))
+            {
+                viewModel.LoginMessage = "Данный логин уже занят";
+            }
+            else
+            {
+                viewModel.LoginMessage = null;  // все проверки логина пройдены
+            }
+
+            if (String.IsNullOrEmpty(formModel.Password))
+            {
+                viewModel.PasswordMessage = "Пароль не может быть пустым";
+            }
+            else if (formModel.Password.Length < 3)
+            {
+                viewModel.PasswordMessage = "Пароль слишком короткий (3 символа минимум)";
+            }
+            else if( ! Regex.IsMatch(formModel.Password, @"\d"))
+            {
+                viewModel.PasswordMessage = "Пароль должен содержать цифры";
+            }
+            else
+            {
+                viewModel.PasswordMessage = null;  // все проверки пароля пройдены
+            }
+
+            // сохранение файла
+            if(formModel.Avatar != null)  // файл передан
+            {
+                /* При приема файла важно:
+                 * - проверить допустимые расширения (тип)
+                 * - проверить максимальный размер
+                 * - заменить имя файла
+                 * Создаем папку (вручную) wwwroot/avatars/
+                 *  и в нее будем сохранять файлы, расширения - оставляем
+                 *  какие есть, а вместо имени - используем GUID
+                 */
+                if(formModel.Avatar.Length > 1048576)
+                {
+                    viewModel.AvatarMessage = "Файл слишком большой (макс 1 МБ)";
+                }
+                // определяем расширение файла
+                String ext = Path.GetExtension(formModel.Avatar.FileName);
+                // проверить расширение на перечень допустимых
+
+                // формируем имя для файла
+                String name = Guid.NewGuid().ToString() + ext;
+
+                formModel.Avatar.CopyTo(
+                    new FileStream("wwwroot/avatars/" + name, FileMode.Create));
+            }
+            return viewModel;
         }
     }
 }
+/* Д.З. Реализовать валидацию всех полей формы, вывод сообщений
+ * Сохранение файла реализовывать только если все поля формы прошли проверку
+ */
---
+++ Models/User/SignUpViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ASP_111.Models.User
+{
+    public class SignUpViewModel
+    {
+        // если null, то проверка прошла успешно, а если есть значение, то
+        // значит это сообщение об ошибке валидации
+        public string? LoginMessage { get; set; }
+        public string? PasswordMessage { get; set; }
+        public string? RepeatMessage { get; set; }
+        public string? RealNameMessage { get; set; }
+        public string? EmailMessage { get; set; }
+        public string? AvatarMessage { get; set; }
+        public string? ConfirmMessage { get; set; }
+
+        public SignUpFormModel? FormModel { get; set; }
+    }
+}
+
+/* Модель для представления - данные для участия в отображении
+ * Классы-модели более предпочтительны, чем множество параметров
+ * ViewBag или ViewData
+ */
